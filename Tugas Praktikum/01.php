a183b668c93872920e1564b45b7f4d24fc1b5a8f
ramansssss <91239554+ramansssss@users.noreply.github.com>
2022-04-09 19:03:35 +0700

    Add files via upload

---
+++ Tugas Praktikum/01.php
@@ -0,0 +1,9 @@
+<?php
+
+$string = "D3 Rekayasa perangkat lunak aplikasi";
+
+echo "<br>" .substr ($string, 3);
+echo "<br>" .substr ($string, -8);
+echo "<br>" .substr ($string, 3,24);
+
+?>
---
+++ Tugas Praktikum/02.php
@@ -0,0 +1,25 @@
+<?php
+$strings = array (
+    'Saya adalah Abdullah',
+    'Saya punya seekor kucing yang sangat lucu',
+    'Setiap hari ku ajak dia bermain' 
+);
+
+$search = array (
+    'Abdullah',
+    'kucing',
+    'lucu',
+    'bermain' 
+);
+
+$replace = array (
+    'Abdurrahman',
+    'bebek',
+    'gemuk',
+    'berenang' 
+);
+$replaced = str_replace ( $search, $replace, $strings );
+foreach ($replaced as $key => $value) {
+echo $value."<br>";
+}
+?>
